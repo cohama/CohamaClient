612842125b856a9188be13d9a85de82e7636d21d
cohama <hirotaka.hamada.1024@gmail.com>
2011-02-23 01:39:24 +0900

    public_timeline を実装した。

--- TwitterApi/TwitterApi.fs
+++ TwitterApi/TwitterApi.fs
@@ -1,5 +1,20 @@
-﻿// F# の詳細 (http://fsharp.net)
-
+﻿[<RequireQualifiedAccess>]
 module TwitterApi
 
 open System.Net
+open System.IO
+open System.Xml
+
+let TwitterApiUrl = "http://api.twitter.com/1/"
+
+let PublicTimeLine () =
+    let path = "statuses/public_timeline.xml"
+    let req = WebRequest.Create( TwitterApiUrl + path )
+    let res = req.GetResponse()
+    use st = res.GetResponseStream()
+    use sr = new StreamReader( st )
+
+    let xmlDoc = XmlDocument()
+    xmlDoc.LoadXml( sr.ReadToEnd() )
+    
+    xmlDoc.SelectNodes( "/statuses/status" );

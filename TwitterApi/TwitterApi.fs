1647bf0a28155c96806df816da6f25b15400f113
cohama <hirotaka.hamada.1024@gmail.com>
2011-02-23 18:33:57 +0900

    home_timeline を実装した。

--- TwitterApi/TwitterApi.fs
+++ TwitterApi/TwitterApi.fs
@@ -1,20 +1,37 @@
-﻿[<RequireQualifiedAccess>]
-module TwitterApi
+﻿
+namespace TwitterApi
 
 open System.Net
 open System.IO
 open System.Xml
+open OAuth
 
-let TwitterApiUrl = "http://api.twitter.com/1/"
+type TwitterApi( oauth:OAuth ) =
 
-let PublicTimeLine () =
-    let path = "statuses/public_timeline.xml"
-    let req = WebRequest.Create( TwitterApiUrl + path )
-    let res = req.GetResponse()
-    use st = res.GetResponseStream()
-    use sr = new StreamReader( st )
+    let TwitterApiUrl = "http://api.twitter.com/1/"
 
-    let xmlDoc = XmlDocument()
-    xmlDoc.LoadXml( sr.ReadToEnd() )
+    member this.PublicTimeLine() =
+        let path = "statuses/public_timeline.xml"
+        let req = WebRequest.Create( TwitterApiUrl + path )
+        let res = req.GetResponse()
+        use st = res.GetResponseStream()
+        use sr = new StreamReader( st )
+
+        let xmlDoc = XmlDocument()
+        xmlDoc.LoadXml( sr.ReadToEnd() )
     
-    xmlDoc.SelectNodes( "/statuses/status" );
+        xmlDoc.SelectNodes( "/statuses/status" );
+
+    member this.HomeTimeLine() =
+        let path = "statuses/home_timeline.xml"
+        let url = TwitterApiUrl + path
+        
+        let res = GetOAuthWebResponse url (oauth.ToAuthorizationHeader url "GET") "GET"
+
+        use st = res.GetResponseStream()
+        use sr = new StreamReader( st )
+
+        let xmlDoc = new XmlDocument()
+        xmlDoc.LoadXml( sr.ReadToEnd() )
+        
+        xmlDoc.SelectNodes( "/statuses/status" )
